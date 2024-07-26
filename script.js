d3ab3bce15a2033609be53a01fef96ae7fb6d30c
Pratik Kumar <161465424+Pratikkumar99@users.noreply.github.com>
2024-07-26 13:32:13 +0530

    new file

--- script.js
+++ script.js
@@ -1,41 +1,69 @@
-let btnIncrease = document.querySelector(".increase");
-let btnDecrease = document.querySelector(".decrease");
-let showQuantity = document.querySelector(".showQuantity");
-let costPrice = document.querySelector(".cost");
-let product =document.querySelector(".product")
-let searchProduct = document.querySelector(".search-product");
-let search = document.querySelector("#search")
+let inputNum = document.querySelector(".input");
+let num1 = document.querySelector(".num1");
+let num2 = document.querySelector(".num2");
+let num3 = document.querySelector(".num3");
+let num4 = document.querySelector(".num4");
+let num5 = document.querySelector(".num5");
+let num6 = document.querySelector(".num6");
+let num7 = document.querySelector(".num7");
+let num8 = document.querySelector(".num8");
+let num9 = document.querySelector(".num9");
+let num0 = document.querySelector(".num0");
+let add = document.querySelector(".add");
+let subs = document.querySelector(".subs");
+let mul = document.querySelector(".mul");
+let div = document.querySelector(".div");
 
-let cost =costPrice.innerHTML;
 
-let textData=1;
-btnIncrease.addEventListener("click",()=>{
-  textData++;
-  costPrice.innerHTML=textData*cost;
-  showQuantity.innerHTML=textData;
-});
-
-btnDecrease.addEventListener("click",()=>{
-    if(showQuantity.innerHTML>1){
-     textData--;
-     costPrice.innerHTML-=cost;
-     showQuantity.innerHTML=textData;
-    }
-});
-
-let foodMenu =[["Fried Catfish","Biscuits & Gravy","Louisiana-Style Red Beans & Rice"],["20$","15$","20$"]];
-
-search.addEventListener("click",()=>{
-  if(searchProduct.value==="Fried Catfish"||"Biscuits & Gravy"||"Louisiana-Style Red Beans & Rice"){
-    product.classList.remove("delete");
-    if(searchProduct.value ==="Fried Catfish"){
-      costPrice.innerHTML=20;
-    }
-    else if(searchProduct.value ==="Biscuits & Gravy"){
-      costPrice.innerHTML=15;
-    }
-  }else{
-    product.classList.add("delete");
-  }
+num1.addEventListener("click", () => {
+  inputNum.innerHTML = inputNum.innerHTML + 1;
 })
 
+num2.addEventListener("click", () => {
+  inputNum.innerHTML = inputNum.innerHTML + 2;
+})
+
+num3.addEventListener("click", () => {
+  inputNum.innerHTML = inputNum.innerHTML + 3;
+})
+
+num4.addEventListener("click", () => {
+  inputNum.innerHTML = inputNum.innerHTML + 4;
+})
+
+num5.addEventListener("click", () => {
+  inputNum.innerHTML = inputNum.innerHTML + 5;
+})
+
+num6.addEventListener("click", () => {
+  inputNum.innerHTML = inputNum.innerHTML + 6;
+})
+
+num7.addEventListener("click", () => {
+  inputNum.innerHTML = inputNum.innerHTML + 7;
+})
+
+num8.addEventListener("click", () => {
+  inputNum.innerHTML = inputNum.innerHTML + 8;
+})
+
+num9.addEventListener("click", () => {
+  inputNum.innerHTML = inputNum.innerHTML + 9;
+})
+
+num0.addEventListener("click", () => {
+  inputNum.innerHTML = inputNum.innerHTML + 0;
+})
+
+add.addEventListener("click",()=>{
+  inputNum.innerHTML=inputNum.innerHTML+"+";
+})
+add.addEventListener("click",()=>{
+  inputNum.innerHTML=inputNum.innerHTML+"+";
+})
+add.addEventListener("click", () => {
+  inputNum.innerHTML = inputNum.innerHTML + "+";
+})
+add.addEventListener("click", () => {
+  inputNum.innerHTML = inputNum.innerHTML + "+";
+})
