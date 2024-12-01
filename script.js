d9ed1fc7264db7ff3a911a76a037f0f33b3e32f2
Pratik Kumar <161465424+Pratikkumar99@users.noreply.github.com>
2024-12-01 18:53:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,23 @@
-let inputNum = document.querySelector(".input");
-let num1 = document.querySelector(".num1");
-let num2 = document.querySelector(".num2");
-let num3 = document.querySelector(".num3");
-let num4 = document.querySelector(".num4");
-let num5 = document.querySelector(".num5");
-let num6 = document.querySelector(".num6");
-let num7 = document.querySelector(".num7");
-let num8 = document.querySelector(".num8");
-let num9 = document.querySelector(".num9");
-let num0 = document.querySelector(".num0");
-let add = document.querySelector(".add");
-let subs = document.querySelector(".subs");
-let mul = document.querySelector(".mul");
-let div = document.querySelector(".div");
+let calc = document.querySelector(".calculator");
+let opert = document.getElementById("opert");
+let blak = document.querySelector(".black");
+let pupl = document.querySelector(".purple");
+let yelo = document.querySelector(".yellow");
+let whit = document.querySelector(".white");
 
-
-num1.addEventListener("click", () => {
-  inputNum.innerHTML = inputNum.innerHTML + 1;
-})
-
-num2.addEventListener("click", () => {
-  inputNum.innerHTML = inputNum.innerHTML + 2;
-})
-
-num3.addEventListener("click", () => {
-  inputNum.innerHTML = inputNum.innerHTML + 3;
-})
-
-num4.addEventListener("click", () => {
-  inputNum.innerHTML = inputNum.innerHTML + 4;
-})
-
-num5.addEventListener("click", () => {
-  inputNum.innerHTML = inputNum.innerHTML + 5;
-})
-
-num6.addEventListener("click", () => {
-  inputNum.innerHTML = inputNum.innerHTML + 6;
-})
-
-num7.addEventListener("click", () => {
-  inputNum.innerHTML = inputNum.innerHTML + 7;
-})
-
-num8.addEventListener("click", () => {
-  inputNum.innerHTML = inputNum.innerHTML + 8;
-})
-
-num9.addEventListener("click", () => {
-  inputNum.innerHTML = inputNum.innerHTML + 9;
-})
-
-num0.addEventListener("click", () => {
-  inputNum.innerHTML = inputNum.innerHTML + 0;
-})
-
-add.addEventListener("click",()=>{
-  inputNum.innerHTML=inputNum.innerHTML+"+";
-})
-add.addEventListener("click",()=>{
-  inputNum.innerHTML=inputNum.innerHTML+"+";
-})
-add.addEventListener("click", () => {
-  inputNum.innerHTML = inputNum.innerHTML + "+";
-})
-add.addEventListener("click", () => {
-  inputNum.innerHTML = inputNum.innerHTML + "+";
-})
+blak.addEventListener("click",()=>{
+    calc.style.backgroundColor = "Black";
+    opert.style.color = "#199ae6e1";
+});
+pupl.addEventListener("click",()=>{
+    calc.style.backgroundColor = "purple";
+    opert.style.color = "#f8eed7e1";
+});
+yelo.addEventListener("click",()=>{
+    calc.style.backgroundColor = "#ff9900e5";
+    opert.style.color = "black";
+});
+whit.addEventListener("click",()=>{
+    calc.style.backgroundColor = "#0d0941e5";
+    opert.style.color = "#d0e420e1";
+});
